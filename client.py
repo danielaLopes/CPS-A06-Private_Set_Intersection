2c17d6b4087a130f5193db07fa9a389d47ca96bb
danielaLopes <danielalopes_97@hotmail.com>
2020-05-24 15:31:06 +0100

    initial commit with project structure

---
+++ client.py
@@ -0,0 +1,15 @@
+class Client:
+    
+    # private input set
+    client_set = {'a', 'b', 'c'}
+    # size of the private input set
+    k = 3
+    def __init__(self):
+
+
+def main():
+    
+    client = Client()
+
+if __name__ == "__main__":
+    main()
---
+++ multiset_operations.py
@@ -0,0 +1,16 @@
+"""from collections import Counter
+counterA = Counter(['a','b','b','c'])
+counterA
+Counter({'b': 2, 'a': 1, 'c': 1})
+lst = [Counter(['a','b']), Counter(['a'])]
+"""
+
+class Operation:
+
+    def __init__(self):
+
+    def union(self):
+
+    def intersection(self):
+
+    def element_reduction(self):
