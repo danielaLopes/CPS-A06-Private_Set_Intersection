30c0806a6bfab07a9a3c1477e89eab92582dd945
danielaLopes <danielalopes_97@hotmail.com>
2020-05-28 11:54:20 +0100

    fixed problem in ttp of global values not updating

--- trusted_third_party.py
+++ trusted_third_party.py
@@ -11,27 +11,8 @@ from multiset_operations import intersection
 
 
 
-#_multiset1 = "test"
-#_multiset2 = "test"
-
-"""class UserMultisets():
-    #__init__(self):
-        #self._multiset1 = ""
-        #self._multiset2 = ""
-
-    @staticmethod
-    submit_multiset(multiset):
-        if self._multiset1 and self._multiset2:
-            self._multiset1 = None
-            self._multiset2 = None
-        if self._multiset1:
-            self._multiset2 = multiset
-        else:
-            self._multiset1 = multiset
-        print("_multiset1:")
-        print(Counter(_multiset1))
-        print("_multiset2:")
-        print(Counter(_multiset2))"""
+_multiset1 = ""
+_multiset2 = ""
 
 
 class RequestHandler(BaseHTTPRequestHandler):  
@@ -48,18 +29,22 @@ class RequestHandler(BaseHTTPRequestHandler):
 
         if _multiset1 != "" and _multiset2 != "": 
 
-            print("_multiset1: " + str(list(_multiset1)))
-            print("_multiset2: " + str(list(_multiset2)))
+            print("_multiset1: " + _multiset1)
+            print("_multiset2: " + _multiset2)
             
             value = query.split("=")[1]
             print("value " + value) 
+
+            map_multiset1 = map(int, _multiset1.strip('[]').split(','))
+            map_multiset2 = map(int, _multiset2.strip('[]').split(','))
+
             if operation == "/union":         
-                result = union(_multiset1, _multiset2, int(value))
+                result = union(map_multiset1, map_multiset2, int(value))
                 self.send_response(200)
                 self.end_headers()
                 self.wfile.write(str(result).encode())
             elif operation == "/intersection":  
-                result = intersection(_multiset1, _multiset1, int(value))
+                result = intersection(map_multiset1, map_multiset2, int(value))
                 self.send_response(200)
                 self.end_headers()
                 self.wfile.write(str(result).encode())
@@ -74,30 +59,25 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         global _multiset1 
         global _multiset2
+
         print("Received POST request for a SUBMIT")
         content_len = int(self.headers.get('Content-Length'))
         json_body = self.rfile.read(content_len)
         json_body_decoded = json_body.decode("utf-8")
         json_multiset = json.loads(json_body_decoded)
         
-        """if _multiset1 != "" and _multiset2 != "":
+        if _multiset1 != "" and _multiset2 != "":
             # resets multisets for next round
             print("reset")
             _multiset1 = ""
             _multiset2 = ""
         if _multiset1 != "":
-            print("if")
-            _multiset2 = map(int, json_multiset["multiset"].strip('[]').split(','))
+            _multiset2 = json_multiset["multiset"]
         else:
-            print("else")
-            _multiset1  =  map(int, json_multiset["multiset"].strip('[]').split(','))"""
-        
-        UserMultisets.submit_multiset(map(int, json_multiset["multiset"].strip('[]').split(',')))
-        
-        """print("_multiset1:")
-        print(Counter(_multiset1))
-        print("_multiset2:")
-        print(Counter(_multiset2))"""
+            _multiset1 = json_multiset["multiset"]
+
+        print("_multiset1:" + _multiset1)
+        print("_multiset2:" + _multiset2)
 
         self.send_response(200)
         self.end_headers()
